f39b7ebe7b4c0c4a804c565f41542ba88fa308ad
Samuel Lorétan <sloretan@riotgames.com>
2016-03-03 16:23:21 -0800

    Fixed a bug that would cause a lot of unnecessary uploads or updates because the status file wasn't updated.

--- utils/factorio_upload_save.sh
+++ utils/factorio_upload_save.sh
@@ -23,14 +23,16 @@ GDRIVE_UTIL="/opt/gdrive --refresh-token $GDRIVE_REFRESH_TOKEN"
 
 # Execute the save
 GDRIVE_FACTORIO_FOLDER_FILE_ID=`cat $FACTORIO_DIR/saves/downloaded_saves | head -n 1`
+echo $GDRIVE_FACTORIO_FOLDER_FILE_ID > "$FACTORIO_DIR/saves/downloaded_saves_new"
 for filename in $FACTORIO_DIR/saves/*.zip; do
+    savename=`basename "$filename"`
+    
     # Check if we knew about this file already.
     found=0
     {
         read;	# Skip the first line
         while read line; do
             storedsavename=`echo $line | cut -d " " -f2`
-            savename=`basename "$filename"`
             if [ "$storedsavename" == "$savename" ]; then
                 found=1
                 fileid=`echo $line | cut -d " " -f1`
@@ -41,17 +43,27 @@ for filename in $FACTORIO_DIR/saves/*.zip; do
     } < "$FACTORIO_DIR/saves/downloaded_saves"
 
     # If we did, update it.
+    storedchecksum=`md5sum $filename | cut -d " " -f1`
     if [ $found == 1 ]; then
         # Verify if the checksum has changed.
-        storedchecksum=`md5sum $filename | cut -d " " -f1`
         if [ "$storedchecksum" == "$checksum" ]; then
             echo $filename has not changed, skipping.
         else
             echo $filename has changed, updating.
             $GDRIVE_UTIL update --no-progress $fileid $filename
         fi
+        
+        # Write to the new status file
+        echo $fileid $savename $checksum >> "$FACTORIO_DIR/saves/downloaded_saves_new"
     else
         echo $filename is a new save, uploading.
-        $GDRIVE_UTIL upload --no-progress --parent $GDRIVE_FACTORIO_FOLDER_FILE_ID $filename
+        fileid=`$GDRIVE_UTIL upload --no-progress --parent $GDRIVE_FACTORIO_FOLDER_FILE_ID $filename | sed -n '2p' | cut -d ' ' -f2`
+        
+        # Add to the status file
+        echo $fileid $savename $storedchecksum >> "$FACTORIO_DIR/saves/downloaded_saves_new"
     fi
 done
+
+# Replace the status file
+rm -f "$FACTORIO_DIR/saves/downloaded_saves"
+mv "$FACTORIO_DIR/saves/downloaded_saves_new" "$FACTORIO_DIR/saves/downloaded_saves"
